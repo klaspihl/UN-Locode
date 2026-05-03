11011a4d1543ebed2dfb81df78e8f5dd92ad19f0
klaspihl <klas.pihl@gmail.com>
2026-05-03 11:32:49 +0200

    Remove files not relevant

--- container/build.cmd
+++
@@ -1,5 +0,0 @@
-cd ..
-docker build --pull --rm -f "container\dockerfile" -t klaspihl/unlocode:latest "src" 
-
-docker push klaspihl/unlocode:latest
-docker rmi $(docker images -f "dangling=true" -q)
